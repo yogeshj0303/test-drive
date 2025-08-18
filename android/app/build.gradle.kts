8b822c98a1fda96e773a695d909a6e654f8ac463
yash <yashshaft@gmail.com>
2025-08-18 12:59:00 +0530

    latest

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 2
-        versionName = "2.0.0"
+        versionCode = 3
+        versionName = "3.0.0"
     }
 
     signingConfigs {
